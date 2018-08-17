e3cea59b783a5836cb42318177a7e983470255c9
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-08-17 15:19:25 +0800

    refactor: デバッグ

--- components/Post/Card.vue
+++ components/Post/Card.vue
@@ -6,6 +6,7 @@
                 :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
                 :srcset="`${post.fields.heroImage.fields.file.url}?w=350&h=196&fit=fill 350w, ${post.fields.heroImage.fields.file.url}?w=1000&h=562&fit=fill 1000w, ${post.fields.heroImage.fields.file.url}?w=2000&h=1125&fit=fill 2000w`"
                 sizes="(min-width: 1024px) 400px, 100vw"
+                v-if="post.fields.heroImage"
             >
         </nuxt-link>
     </div>
@@ -13,7 +14,10 @@
         <div class="media">
             <div class="media-left">
                 <figure class="image is-48x48 is-circle">
-                    <img :src="post.fields.author.fields.image.fields.file.url" alt="Image">
+                    <img
+                        :src="post.fields.author.fields.image.fields.file.url"
+                        alt="Image"
+                    >
                 </figure>
             </div>
             <div class="media-content">

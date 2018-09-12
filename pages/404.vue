69c02ecaa29e508876a269f8366fcd3aab036e12
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-09-12 14:50:26 +0800

    refactor: 404エラーページの修正

--- pages/404.vue
+++ pages/404.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="container">
     <section class="section has-text-centered">
-        <h1 class="title is-1">４０４</h1>
         <img src="~/assets/img/icon-broken-link-512.png" class="broken-link">
+        <h1 class="title is-1 m-t-50">４０４</h1>
         <p class="subtitle has-text-grey m-t-30">
             指定されたページが見つかりませんでした。<br>
             既に削除されたか、URLが間違っている可能性があります。

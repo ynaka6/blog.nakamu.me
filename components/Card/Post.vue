1dc94c912bcead6ffdf1559fe1c2207b45a40f10
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-09-25 23:10:06 +0800

    refactor: エラー調査

--- components/Card/Post.vue
+++ components/Card/Post.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="card large">
-
+        <div class="card-image">
+            <nuxt-link class="image" :to="{ name: 'posts-slug', params: { slug: post.fields.slug }}">
+            <img class="thumbnail"
+                    :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
+                    :srcset="`${post.fields.heroImage.fields.file.url}?w=350&h=196&fit=fill 350w, ${post.fields.heroImage.fields.file.url}?w=1000&h=562&fit=fill 1000w, ${post.fields.heroImage.fields.file.url}?w=2000&h=1125&fit=fill 2000w`"
+                    sizes="(min-width: 1024px) 400px, 100vw"
+                    v-if="post.fields.heroImage"
+                >
+                <span class="image__category-label" v-text="post.fields.category"></span>
+            </nuxt-link>
+        </div>
     </div>
 </template>
 

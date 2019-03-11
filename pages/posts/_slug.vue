364a47dd0f75b6db7d9bc9a31a8d2988d2da905a
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2019-03-11 15:06:00 +0800

    refactor: Lighthouseの修正

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -105,7 +105,11 @@
 
                 <div class="is-clearfix">
                   <div v-if="prevPost" class="is-pulled-left">
-                    <nuxt-link class="button is-large is-circle is-light" :to="`/posts/${prevPost.fields.slug}/`">
+                    <nuxt-link
+                      class="button is-large is-circle is-light"
+                      :to="`/posts/${prevPost.fields.slug}/`"
+                      :aria-label="prevPost.fields.title"
+                    >
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1792 1792" fill="currentColor"><path d="M1203 544q0 13-10 23l-393 393 393 393q10 10 10 23t-10 23l-50 50q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l50 50q10 10 10 23z"/></svg>
                     </nuxt-link>
                     <p class="is-size-7 m-t-5">
@@ -113,7 +117,11 @@
                     </p>
                   </div>
                   <div v-if="nextPost" class="is-pulled-right">
-                    <nuxt-link class="button is-large is-circle is-light" :to="`/posts/${nextPost.fields.slug}/`">
+                    <nuxt-link
+                      class="button is-large is-circle is-light"
+                      :to="`/posts/${nextPost.fields.slug}/`"
+                      :aria-label="nextPost.fields.title"
+                    >
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1792 1792" fill="currentColor"><path d="M1171 960q0 13-10 23l-466 466q-10 10-23 10t-23-10l-50-50q-10-10-10-23t10-23l393-393-393-393q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l466 466q10 10 10 23z"/></svg>
                     </nuxt-link>
                     <p class="is-size-7 m-t-5">
